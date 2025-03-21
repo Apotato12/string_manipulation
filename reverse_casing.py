829ab2cc3922ac6320bd1db16a39d4ef35155154
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-21 23:55:07 +0800

    followed_PEP8_standards_and_added_comments

--- reverse_casing.py
+++ reverse_casing.py
@@ -1,5 +1,12 @@
-fullname = input("Please enter your full name in incorrect casing: ")
+# Program 6 Batch 5
+# Atienza, Rein Gabriel
+# BSCPE 1-2
+def reverse_casing():
+    """asks the user to input their full name in incorrect casing and prints each character with reversed casing."""
+    fullname = input("Please enter your full name in incorrect casing: ")
 
-reverse_casing = "".join(char.swapcase() for char in fullname)
+    reverse_casing = "".join(char.swapcase() for char in fullname)
 
-print("your name is : " + reverse_casing)
+    print("your name is : " + reverse_casing)
+
+reverse_casing()
