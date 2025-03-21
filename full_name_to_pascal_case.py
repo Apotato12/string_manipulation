7a40fca5c7efb25b269e8da881408c396c9f347d
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-22 00:11:02 +0800

    pseudocode

---
+++ full_name_to_pascal_case.py
@@ -0,0 +1,5 @@
+#ask the user to insert their full name in incorrect casing
+#use split to split the full name
+#capitalize to capitalize the first letter of the splited name
+#use join to join them back
+#print the full name
