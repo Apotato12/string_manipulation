5015e7d29bf8e6037bfbc79f19f136ca99a6a2c5
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-21 23:49:59 +0800

    followed_PEP8_standards_and_added_comments

--- proper_casing.py
+++ proper_casing.py
@@ -1,5 +1,13 @@
-fullname = input("Please enter your full name in incorrect casing: ")
+# Program 5 Batch 5
+# Atienza, Rein Gabriel
+# BSCPE 1-2
 
-correct_casing = fullname.title()
+def proper_casing():
+    """Prompts the user to input their full name in incorrect casing and prints it in proper casing."""
+    fullname = input("Please enter your full name in incorrect casing: ")
+    
+    correct_casing = fullname.title()
+    
+    print("Your name is:", correct_casing)
 
-print("Your name is:", correct_casing)
+proper_casing()
