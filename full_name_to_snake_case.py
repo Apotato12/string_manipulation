8074530e934528078afa1e8355d317b93268aed1
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-22 00:17:11 +0800

    pseudocode

---
+++ full_name_to_snake_case.py
@@ -0,0 +1,5 @@
+#ask the user to input their full name in incorrect casing
+#use split to split the name into words
+#use lower to conver the words into lowercase
+#use join with "_" to join them with an underscore
+#print the name
