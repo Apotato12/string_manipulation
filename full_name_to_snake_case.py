68080fcf2b66aaef060066ff04d040046135c90b
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-22 00:19:40 +0800

    followed_PEP8_standards_and_added_comments

--- full_name_to_snake_case.py
+++ full_name_to_snake_case.py
@@ -1,5 +1,13 @@
-fullname = input("Please enter your full name in incorrect casing: ")
+# Program 10 Batch 5
+# Atienza, Rein Gabriel
+# BSCPE 1-2
 
-snake = '_'.join(word.lower() for word in fullname.split())
+def snake_case():
+    """Prompts the user to input their full name in incorrect casing and prints it in snake case."""
+    fullname = input("Please enter your full name in incorrect casing: ")
+    
+    snake = '_'.join(word.lower() for word in fullname.split())
+    
+    print("Your name in snake case is: " + snake)
 
-print("Your name in snake case is:" + snake)
+snake_case()
