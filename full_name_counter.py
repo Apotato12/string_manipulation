60057913fb952a2c074f0b9dca4c06bbff1c7f1a
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-22 00:07:21 +0800

    followed_pep8_standards_and_added_comments

--- full_name_counter.py
+++ full_name_counter.py
@@ -1,5 +1,13 @@
-fullname = input("Please enter your full name: ")
+# Program 8 Batch 5
+# Atienza, Rein Gabriel
+# BSCPE 1-2
 
-letter = len(fullname)
+def characters_in_fullname():
+    """Prompts the user to input their full name and prints the number of characters in the input."""
+    fullname = input("Please enter your full name: ")
+    
+    letter = len(fullname) 
+    
+    print("Number of characters in the full name:", letter)
 
-print("Number of characters in the full name:", letter)
+characters_in_fullname()
