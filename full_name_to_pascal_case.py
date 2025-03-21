badfa03b55b67b605be45a2bd4cefb41ae52b030
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-22 00:14:32 +0800

    followed_PEP8_standards_and_added_comments

--- full_name_to_pascal_case.py
+++ full_name_to_pascal_case.py
@@ -1,5 +1,13 @@
-fullname = input("Please enter your full name in incorrect casing: ")
+# Program 9 Batch 5
+# Atienza, Rein Gabriel
+# BSCPE 1-2
 
-capitalization = ''.join(word.capitalize() for word in fullname.split())
+def pascal_case():
+    """Prompts the user to input their full name in incorrect casing and prints it in Pascal Case."""
+    fullname = input("Please enter your full name in incorrect casing: ")
+    
+    capitalization = ''.join(word.capitalize() for word in fullname.split())
+    
+    print("Your name in Pascal Case is: " + capitalization)
 
-print("Your name in Pascal Case is:" + capitalization)
+pascal_case()
