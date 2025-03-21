e62d3b13be41db8914857b65310b913c4d45e698
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-21 23:44:51 +0800

    followed_PEP8_standards_and_added_comments

--- full_name_lower_case.py
+++ full_name_lower_case.py
@@ -1,6 +1,13 @@
-fullname = input("Please enter your full name: ")
+# Program 4 Batch 5
+# Atienza, Rein Gabriel
+# BSCPE 1-2
 
-lower_case = fullname.lower()
+def lowercase():
+    """Prompts the user to input their full name and prints it in all lowercase."""
+    fullname = input("Please enter your full name: ")
+    
+    lower_case = fullname.lower()
+    
+    print("Your name is: " + lower_case)
 
-print("Your name is: " + lower_case)
-
+lowercase()
