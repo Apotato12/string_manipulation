1e595d7cd04fbfa14aa76b10af7a978a9215e704
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-21 23:26:00 +0800

    followed_PEP8_standards_and_added_comments

--- full_name_with_spaces.py
+++ full_name_with_spaces.py
@@ -1,4 +1,12 @@
-fullname = input("Please enter your full name: ")
-remove_spaces = fullname.lstrip()
+# Program 1 Batch 5
+# Atienza, Rein Gabriel
+# BSCPE 1-2
 
-print("your name is: " + remove_spaces)
+def name():
+    """Prompts the user to input their full name and prints it without leading spaces."""
+    fullname = input("Please enter your name with spaces on the start: ")
+    space_remove = fullname.lstrip()
+
+    print("Your name is: " + space_remove)
+
+name()
