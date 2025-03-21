7e74020fd0f0248ace48884ec72209dc54b7ebc6
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-21 23:34:18 +0800

    followed_PEP8_standards_and_added_comments

--- numbers_with_6_digits.py
+++ numbers_with_6_digits.py
@@ -1,6 +1,14 @@
-number = int(input("Please enter a number (0-1000): "))
+# Program 2 Batch 5
+# Atienza, Rein Gabriel
+# BSCPE 1-2
 
-if 0 <= number <= 1000:
-    formatted_number = f"{number:06}"
+def number_6_digits():
+    """Prompts the user to input a number (0-1000) and prints it in 6-digit format."""
+    number = int(input("Please enter a number (0-1000): "))
     
-    print("Formatted number:", formatted_number)
+
+    if 0 <= number <= 1000:
+        formatted_number = f"{number:06}"
+        print("Formatted number:", formatted_number)
+
+number_6_digits()
